a62bc3fac424dc69054a5cb54412cbe2423962a7
AmanKamble <amankamble88@gmail.com>
2023-08-16 16:19:48 +0530

    hidding the title input
    hidding the input whenever we click outside the textarea

--- src/Components/CreateArea.jsx
+++ src/Components/CreateArea.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import Zoom from '@mui/material/Zoom';
 import AddIcon from '@mui/icons-material/Add';
 import Fab from '@mui/material/Fab';
@@ -9,12 +9,31 @@ const CreateArea = (props) => {
         title: "",
         content: ""
     });
+    const createAreaRef = useRef(null);
+
+    useEffect(() => {
+        // Attach click event listener to the document
+        const handleClickOutside = (event) => {
+            if (createAreaRef.current && !createAreaRef.current.contains(event.target)) {
+                setExpanded(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            // Clean up the click event listener when component unmounts
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
     function handleChange(event) {
         const { name, value } = event.target;
         setNote(prevNote => {
             return { ...prevNote, [name]: value };
         });
     }
+
     function submitNote(event) {
         props.onAdd(note);
         setNote({
@@ -23,8 +42,9 @@ const CreateArea = (props) => {
         });
         event.preventDefault();
     }
+
     return (
-        <form className="create-note">
+        <form className="create-note" ref={createAreaRef}>
             {isExpanded && (
                 <input
                     name="title"
@@ -33,23 +53,22 @@ const CreateArea = (props) => {
                     placeholder="Title"
                 />
             )}
-
             <textarea
                 name="content"
                 onClick={() => setExpanded(true)}
                 onChange={handleChange}
                 value={note.content}
+                onFocus={() => setExpanded(true)}
                 placeholder="Take a note..."
                 rows={isExpanded ? 3 : 1}
             />
-
             <Zoom in={isExpanded}>
                 <Fab onClick={submitNote}>
                     <AddIcon />
                 </Fab>
             </Zoom>
         </form>
-    )
+    );
 }
 
-export default CreateArea
+export default CreateArea;
