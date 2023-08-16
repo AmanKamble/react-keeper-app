fe3a3c115022cc2044e9a9e6b3ee869fbe20246d
AmanKamble <amankamble88@gmail.com>
2023-08-16 16:04:52 +0530

    CreateArea component
     CreateArea component is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import CreateArea from "./Components/CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
   return (
     <div>
       <Header />
+      <CreateArea />
       <Footer />
     </div>
   )
